029ce4851262aed4a46edfd47e17a92280d6cf74
MasuCatz <bloodteam.a.ranger@gmail.com>
2019-09-12 00:56:59 +0900

    .net core MVC Hello World

---
+++ Something/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Something.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+			ViewData["msg"] = "Hello,World!";
+            return View();
+        }
+    }
+}
